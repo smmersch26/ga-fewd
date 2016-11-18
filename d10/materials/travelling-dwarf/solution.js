eaa93e82dc7c49c2e8df4b6dcad2a175479ab27e
smmersch26 <smmersch26@gmail.com>
2016-11-17 21:28:23 -0500

    dwarf-finish-1

---
+++ d10/materials/travelling-dwarf/solution.js
@@ -0,0 +1,62 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+
+var x = 0
+
+while (x < 4){
+	d.move();
+	x = x + 1;
+}
+
+d.orient('down');
+
+var x = 0
+
+while (x < 16){
+	d.move();
+	x = x + 1;
+}
+
+d.orient('right');
+
+var x = 0
+
+while (x < 6){
+	d.move();
+	x = x + 1;
+}
+
+d.orient('up');
+
+var x = 0
+
+while (x < 11){
+	d.move();
+	x = x + 1;
+}
+
+d.orient('right');
+
+var x = 0
+
+while (x < 6){
+	d.move();
+	x = x + 1;
+}
+
+d.orient('down');
+
+var x = 0
+
+while (x < 9){
+	d.move();
+	x = x + 1;
+}
+
+d.orient('right');
+
+var x = 0
+
+while (x < 2){
+	d.move();
+	x = x + 1;
+}
