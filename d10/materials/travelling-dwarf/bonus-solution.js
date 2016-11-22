4c3bafe83a55a8425af58e4b2595154cca09ad97
smmersch26 <smmersch26@gmail.com>
2016-11-22 18:33:53 -0500

    d10 hey i tried

---
+++ d10/materials/travelling-dwarf/bonus-solution.js
@@ -0,0 +1,15 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+
+var x = 0;
+
+while (x < 20){
+
+	d.move();
+	x = x + 1;
+
+	var color = "color grey"
+
+	if (x + 1 === "color grey"){
+		d.orient("down")
+	}
+}
